f40982365faefd96b34321015d8d377864a9beee
Omid Kayhani <o.kayhani@gmail.com>
2023-06-18 14:44:28 +0000

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching (drivers, driverName) {
+    return drivers.filter(driver => driver.toUpperCase() === driverName.toUpperCase());
+}
+
+function fuzzyMatch (drivers, initial) {
+    return drivers.filter(driver => driver.toUpperCase().startsWith(initial.toUpperCase()));
+}
+
+function matchName (drivers, name) {
+    const upperName = name.toUpperCase();
+    return drivers.filter(driver => driver.name.toUpperCase() === upperName);
+}
